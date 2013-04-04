bee832d4a4337044ee7776a8454ff75d7c43e27f
tr <riess@w3-data.de>
2013-04-04 16:11:19 +0200

    use net again

--- readlogger.py
+++ readlogger.py
@@ -373,12 +373,15 @@ def main():
     v1.update(v2)
     m = r.preparemessage(v1, False)
     logging.getLogger("meteorolopi").debug("Read and prepared message")
+    
+    url = settings.SEND_TO_URL
+    params = {"text": m, "from": settings.SMS_FROM_NUMBER}
+    requests.get(url, params=params)
+    logging.getLogger("meteorolopi").debug("Sent to URL")
+    
     s = sms.SMS(settings.SMS_TO_NUMBER, settings.SMS_FROM_NUMBER)
-    logging.getLogger("meteorolopi").debug("About to send message")
-    s.send(m)
-    # url = settings.SEND_TO_URL
-    # params = {"text": m, "from": settings.SMS_FROM_NUMBER}
-    # requests.get(url, params=params)
+    logging.getLogger("meteorolopi").debug("About to send message (not actually sending, just test)")
+    # s.send(m)
 
 if __name__ == '__main__':
     main()
